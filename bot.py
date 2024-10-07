a29af45501bb758b330d8042e8e441d7c24fb969
Moses-93 <moieseenko2001@gmail.com>
2024-10-07 20:12:25 +0300

    logging added

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 import asyncio
+import logging
+import sys
 
 from aiogram import Bot, Dispatcher
 from os import getenv
 from bot.handlers import router
 
+
+logging.basicConfig(
+    level=logging.INFO,  # Рівень логування (INFO для загальної інформації)
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Формат повідомлень
+    handlers=[
+        logging.FileHandler("logs/bot.log"),  # Файл для зберігання логів
+        logging.StreamHandler(),  # Виведення логів у консоль
+    ],
+)
+
+logger = logging.getLogger(__name__)
 
 TOKEN = getenv("TOKEN")
 
@@ -17,4 +30,5 @@ async def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
