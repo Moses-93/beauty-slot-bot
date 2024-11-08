577ddabddf1f7b1e7e173a014846d7204f665bd4
Moses-93 <moieseenko2001@gmail.com>
2024-11-08 12:22:33 +0200

    Fixed errors when sending a reminder

--- utils/booking_reminders.py
+++ utils/booking_reminders.py
@@ -1,11 +1,14 @@
+import logging
 from db.db_reader import GetNotes
 from datetime import datetime, timedelta
-from utils.format_datetime import NowDatetime
 from utils.message_templates import template_manager
 from utils.message_sender import manager
 
+logger = logging.getLogger(__name__)
 
-async def find_time_for_reminder(now=datetime.now()):
+
+async def find_time_for_reminder():
+    now = datetime.now()
     active_notes = await GetNotes(only_active=True).get_notes()
 
     for note in active_notes:
@@ -18,4 +21,5 @@ async def find_time_for_reminder(now=datetime.now()):
 
         if abs((now - reminder_time).total_seconds()) <= 600:  # 10 хвилин в секундах
             msg = template_manager.recording_reminder(note)
+            logger.info(f"Нагадування відправлено користувачу: {note.username}")
             await manager.send_message(chat_id=note.user_id, message=msg)
