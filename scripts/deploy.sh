3e8e2e21ab700d251dcdeaac2991b82e9a0b89cc
Moses-93 <moieseenko2001@gmail.com>
2024-11-15 14:59:30 +0200

    added logs to scriprs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
-cd /root
-cd app/chashurina-brows-bot/
+#!/bin/bash
+echo "Current directory: $(pwd)"
+cd /root || exit
+echo "Changed to /root: $(pwd)"
+cd app/chashurina-brows-bot/ || exit
+echo "Changed to app directory: $(pwd)"
 docker compose down
 docker pull moses93/telegram-bot:latest
 docker compose up -d --remove-orphans
