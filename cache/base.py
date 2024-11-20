0876ce9373f25f0ffef4a8a664675dcb33ef860d
Moses-93 <moieseenko2001@gmail.com>
2024-11-20 19:02:45 +0200

    added an abstraction to the base class for easy scaling

--- cache/base.py
+++ cache/base.py
@@ -1,39 +1,51 @@
-from aiocache import caches, Cache
-from aiocache.plugins import HitMissRatioPlugin
 import logging
+from aiocache import caches, Cache
+from abc import ABC, abstractmethod
+
 
 logger = logging.getLogger(__name__)
 
-caches.set_config({
-    "default": {
-        "cache": "aiocache.SimpleMemoryCache",  # Простий кеш
-        "ttl": 1800,
-        "plugins": [
-            {"class": "aiocache.plugins.HitMissRatioPlugin"}
-        ]
+caches.set_config(
+    {
+        "default": {
+            "cache": "aiocache.SimpleMemoryCache",  # Простий кеш
+            "ttl": 1800,
+            "plugins": [{"class": "aiocache.plugins.HitMissRatioPlugin"}],
+        }
     }
-})
+)
 
 
-class BaseCache:
+class ICache(ABC):
+    
+    @abstractmethod
+    async def get(self, key:str):
+        pass
+
+    @abstractmethod
+    async def set(self, key:str, value):
+        pass
+
+    @abstractmethod
+    async def delete(self, key:str):
+        pass
+
+
+class AioCacheAdapter(ICache):
 
     def __init__(self, caches_name="default") -> None:
-        self._cache: Cache = caches.get(caches_name)
+        self._cache = caches.get(caches_name)
 
-    async def get_cache(self, key:str):
-        if key:
-            try:
-                return await self._cache.get(key)
-            except KeyError:
-                logger.error(f"Помилка доступу до кешу: {key}")
-        return await self._cache.get("global_dict")
+    async def get(self, key: str):
+        try:
+            return await self._cache.get(key)
+        except KeyError:
+            logger.error(f"Помилка доступу до кешу: {key}")
 
-    async def set_cache(self, key:str|int, value):
+    async def set(self, key: str | int, value):
         await self._cache.set(key, value)
         logger.info(f"Кеш оновлено: {key} -> {value}")
-    
-    async def delete_cache(self, key:str|int):
+
+    async def delete(self, key: str | int):
         await self._cache.delete(key)
         logger.debug(f"Ключ видалено з кешу: {key}")
-
-            
--- cache/cache.py
+++ cache/cache.py
@@ -1,56 +1,58 @@
 import asyncio
-from .base import BaseCache
+from .base import AioCacheAdapter, ICache
 
 
-class UserCache(BaseCache):
-    def __init__(self,  cache_name="default"):
-        super().__init__(cache_name)
+class UserCache:
+    def __init__(self, cache: ICache):
+        self.cache = cache
 
-    async def init_user(self, user_id:int):
-        if not await self._cache.exists(user_id):
-            await self.set_cache(user_id, {})
+    async def init_user(self, user_id: int):
+        if not await self.cache.get(user_id):
+            await self.cache.set(user_id, {})
 
-    async def get_user_cache(self, user_id, *keys):
-        user_data = await self.get_cache(key=user_id)
+    async def get_user_cache(self, user_id:int, *keys):
+        user_data = await self.cache.get(key=user_id)
         if not keys:
             return user_data
         return tuple(user_data.get(key) for key in keys)
-        
+
     async def set_user_cache(self, user_id: int, **kwargs):
         await self.init_user(user_id)
-        user_data = await self.get_cache(key=user_id)
+        user_data = await self.cache.get(key=user_id)
         user_data.update(kwargs)
-        await self.set_cache(user_id, user_data)
-    
-    async def clear_user_cache(self, user_id: int, *save_keys):
-        user_data = await self.get_cache(key=user_id)
-        if not user_data:
-            return
+        await self.cache.set(user_id, user_data)
+
+    async def clear_user_cache(self, user_id: int, *save_keys:str):
         if save_keys:
-            user_data = {key: value for key, value in user_data.items() if key in save_keys}
-            await self.set_cache(user_id, user_data)
+            user_data = await self.cache.get(key=user_id)
+            user_data = {
+                key: value for key, value in user_data.items() if key in save_keys
+            }
+            await self.cache.set(user_id, user_data)
         else:
-            await self.delete_cache(key=user_id)
+            await self.cache.delete(key=user_id)
 
 
-class RequestCache(BaseCache):
-    def __init__(self, cache_name="default") -> None:
-        super().__init__(cache_name)
+class RequestCache:
+    def __init__(self, cache: ICache) -> None:
+        self.cache = cache
 
-    async def get_request(self, key:str=None, keys:list[str]=None):
+    async def get_request(self, key: str = None, keys: list[str] = None):
         if key:
-            return await self.get_cache(key)
-        return await asyncio.gather(*(self.get_cache(k) for k in keys))
-        
-    async def set_request(self, key:str=None, value=None, **kwargs):
-        if kwargs:
-            await asyncio.gather(*(self.set_cache(k, v) for k, v in kwargs.items()))
-        await self.set_cache(key=key, value=value)
-    
-    async def clear_request(self, key:str=None, keys:list[str]=None):
-        if keys:
-            await asyncio.gather(*(self.delete_cache(k) for k in keys))
-        await self.delete_cache(key=key)
+            return await self.cache.get(key)
+        return await asyncio.gather(*(self.cache.get(k) for k in keys))
 
-user_cache = UserCache()
-request_cache = RequestCache()
+    async def set_request(self, key: str = None, value=None, **kwargs):
+        if kwargs:
+            await asyncio.gather(*(self.cache.set(k, v) for k, v in kwargs.items()))
+        await self.cache.set(key=key, value=value)
+
+    async def clear_request(self, key: str = None, keys: list[str] = None):
+        if keys:
+            await asyncio.gather(*(self.cache.delete(k) for k in keys))
+        await self.cache.delete(key=key)
+
+
+cache = AioCacheAdapter()
+user_cache = UserCache(cache)
+request_cache = RequestCache(cache)
