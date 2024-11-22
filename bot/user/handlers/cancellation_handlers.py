0316d5ed88095d74b263301c4c61c59b535e9e0e
Moses-93 <moieseenko2001@gmail.com>
2024-11-22 15:49:06 +0200

    The method of receiving data on canceled reservations has been changed

--- bot/user/handlers/cancellation_handlers.py
+++ bot/user/handlers/cancellation_handlers.py
@@ -1,6 +1,5 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
-from user_data import get_user_data
 import logging
 from utils.message_templates import template_manager
 from utils.message_sender import manager
@@ -12,18 +11,16 @@ logger = logging.getLogger(__name__)
 
 @cancellation_router.callback_query(lambda c: c.data.startswith("note_"))
 async def cancel_booking(callback: CallbackQuery, user_id):
-    note_id = int(callback.data.split("_")[1])
-    (note,) = get_user_data(user_id, "note")
-    logger.info(f"Користувач з ID: {user_id} скасував запис на: {note}")
-    msg_for_master = template_manager.message_to_the_master(
-        username=note.username,
-        service=note.service,
-        date=note.free_date,
-        time=note.time,
+    _, note_id, name, date, time = callback.data.split("_")
+    msg_for_master = await template_manager.message_to_the_master(
+        username=name,
+        date=date,
+        service=None,
+        time=time,
         booking_cancel=True,
     )
     await manager.send_message(user_id, message=msg_for_master)
-    await notes_manager.delete(note_id=note_id)
+    await notes_manager.delete(note_id=int(note_id))
     msg = template_manager.get_cancel_notification()
     await callback.message.answer(text=msg)
     await callback.answer()
