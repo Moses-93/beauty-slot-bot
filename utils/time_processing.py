45b979d22514c63035007b525d3d5455b6a3c082
Moses-93 <moieseenko2001@gmail.com>
2024-11-22 15:41:06 +0200

    Logs have been specified. The number of database queries has been reduced

--- utils/time_processing.py
+++ utils/time_processing.py
@@ -1,11 +1,10 @@
 import logging
-from utils.format_datetime import NowDatetime, FormatDate, FormatTime
-from user_data import get_user_data
+from utils.format_datetime import FormatDate, FormatTime
+from cache.cache import user_cache
 from db.db_reader import GetNotes
 from datetime import datetime
 
 
-current_date_time = NowDatetime()
 format_date = FormatDate()
 format_time = FormatTime()
 
@@ -14,57 +13,57 @@ logger = logging.getLogger(__name__)
 
 
 async def get_busy_slots(user_id: int):
-    service, date = get_user_data(user_id, "service", "date")
+    logger.info("Запуск функції для пошуку зайнятих слотів")
+    service, date = await user_cache.get_user_cache(user_id, "service", "date")
     busy_slots = []
     notes = await GetNotes(date_id=date.id).get_notes()
-    logger.info(f"NOTES(in get_busy_slots): {notes}")
     for time in notes:
         time = datetime.combine(date.date, time.time)
         end_time = time + service.durations
         start_time = time - service.durations
         busy_slots.append({"start": start_time, "end": end_time})
         busy_slots = sorted(busy_slots, key=lambda slot: slot["start"])
-    logger.info(f"get_busy_slots ПОВЕРНУВ {busy_slots}")
+    logger.info(f"Повернуто: {busy_slots}")
     return busy_slots
 
 
 async def find_nearest_available_time(user_id: int, time: datetime, busy_slots: list):
-    (service,) = get_user_data(user_id, "service")
+    logger.info("Запуск функції для знаходження найближчого доступного часу")
+    (service,) = await user_cache.get_user_cache(user_id, "service")
     current_time = time
-    logger.info(f"CURRENT_TIME(in find_nearest_available_time): - {current_time}")
+    logger.info(f"CURRENT_TIME: - {current_time}")
 
     for busy_slot in busy_slots:
-        logger.info(f"СТАРТОВИЙ ЧАС: {busy_slot['start'].time()}")
-        logger.info(f"КІНЦЕВИЙ ЧАС: {busy_slot['end'].time()}")
 
         if busy_slot["start"] <= current_time <= busy_slot["end"]:
             # Якщо вибраний час зайнятий, переносимо на кінець зайнятого часу
             current_time = busy_slot["end"]
         # Перевіряємо, чи є достатньо часу після поточного часу
     if current_time + service.durations <= busy_slot["start"]:
-        logger.info(f"find_nearest_available_time ПОВЕРНУВ: {current_time}")
+        logger.info(f"Повернуто: {current_time}")
         return current_time  # Повертаємо найближчий доступний час
-    logger.info(f"find_nearest_available_time ПОВЕРНУВ {current_time}")
+    logger.info(f"Повернуто: {current_time}")
     return current_time
 
 
 async def check_slot(user_id: int, time: datetime):
+    logger.info("Запуск функції для перевірки доступності часу")
     busy_slots = await get_busy_slots(user_id)
     if busy_slots:
-        logger.info(f"TIME(in check_slot) -- {time}")
+        logger.info(f"Час:{time}")
         for slot in busy_slots:
             if slot["start"] <= time <= slot["end"]:
-                logger.info(f"check_slot ВИКЛИКАВ find_nearest_available_time")
                 return await find_nearest_available_time(user_id, time, busy_slots)
 
-        logger.info(f"check_slot ПОВЕРНУВ {time}")
+        logger.info(f"Повернуто: {time}")
         return time
     else:
-        logger.info(f"check_slot ПОВЕРНУВ {time}")
+        logger.info(f"Повернуто: {time}")
         return time
 
 
 async def time_check(date_time: datetime):
-    now = current_date_time.now_datetime()
+    now = datetime.now()
     if date_time < now:
+        logger.warning("Час, який обрав користувач вже пройшов")
         return False
--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,11 @@
-from datetime import datetime
 import logging
+from datetime import datetime
 from db.db_writer import notes_manager
 from bot.user.keyboards.booking_keyboard import confirm_time_keyboard
-from user_data import get_user_data
+from cache.cache import user_cache
 from .time_processing import check_slot, time_check, format_time
 from .message_sender import manager
 from .message_templates import template_manager
-from db.models import FreeDate, Service
 from os import getenv
 
 
@@ -16,16 +15,11 @@ USER_ID = getenv("USER_ID_ADMIN")
 logger = logging.getLogger(__name__)
 
 
-async def promote_booking(
-    name: str,
-    username: str,
-    time: datetime,
-    date: FreeDate,
-    service: Service,
-    user_id: int,
-):
+async def promote_booking(user_id: int, time: datetime):
     """Проміжна функція для запису в БД та відправлення повідомлень користувачу та майстру"""
-    logger.info(f"TIME(in promote_booking): {time} | TYPE: {type(time)}")
+    name, username, service, date = await user_cache.get_user_cache(
+        user_id, "name", "username", "service", "date"
+    )
     current_time = datetime.now()
     await notes_manager.create(
         name=name,
@@ -36,41 +30,35 @@ async def promote_booking(
         user_id=user_id,
         created_at=current_time,
     )
-    msg_for_master = template_manager.message_to_the_master(
+    msg_for_master = await template_manager.message_to_the_master(
         username,
-        service,
-        date,
+        service.name,
+        date.date,
         time.time(),
     )
     await manager.send_message(USER_ID, msg_for_master)
-    msg_for_user = template_manager.get_booking_confirmation(service, date, time.time())
-    logger.info(f"USER ID: {user_id}")
+    msg_for_user = await template_manager.get_booking_confirmation(
+        user_id=user_id,
+        time=time.time(),
+    )
     await manager.send_message(user_id, msg_for_user)
 
 
 async def handlers_time(user_id: int, time: str):
-    name, username, date, service = get_user_data(
-        user_id, "name", "username", "date", "service"
-    )
-    logger.info(
-        f"(in handlers_time)NAME - {name}, USERNAME - {username}, DATE - {date}, SERVICE - {service}"
-    )
+
+    (date,) = await user_cache.get_user_cache(user_id, "date")
     time = format_time.formats_time_str_to_datetime(time).time()
     time = datetime.combine(date.date, time)
     if await time_check(time) == False:
         message = template_manager.elapsed_time_warning(time)
-        logger.warning("handlers_time - Час, який обрав користувач вже пройшов")
         return False, message
-
     nearest_time = await check_slot(user_id, time)
-    logger.info(f"nearest_time {nearest_time}")
-    logger.info(f"time {time}")
-
     if nearest_time == time:
-        logger.info(
-            f"handlers_time ВИКЛИКАВ promote_booking ДЛЯ ЗАПИСУ В БД ТА ВІДПРАЛЕННЯ ПОВІДОМЛЕННЯ"
+        logger.info("Викликано функцію для запису в БД та відправки повідомлень")
+        await promote_booking(
+            user_id=user_id,
+            time=time,
         )
-        await promote_booking(name, username, time, date, service, user_id)
 
     else:
         keyboard = confirm_time_keyboard(nearest_time)
