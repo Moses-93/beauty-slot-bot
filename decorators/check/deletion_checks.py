bc3bb8357acbc2adf23e33061892d357a0f90224
Moses-93 <moieseenko2001@gmail.com>
2024-11-22 15:45:30 +0200

    Improved structure of the decorator

--- decorators/check/deletion_checks.py
+++ decorators/check/deletion_checks.py
@@ -1,40 +1,44 @@
+import logging
 from aiogram.types import CallbackQuery
 from db.db_reader import GetNotes
 from bot.admin.keyboards.date_keyboard import delete_date_keyboard
 from bot.admin.keyboards.service_keybord import delete_service_keyboard
 from utils.message_templates import template_manager
+from cache.cache import request_cache
+
+logger = logging.getLogger(__name__)
 
 
-def prevent_deletion_if_related(date: bool = False, service: bool = False):
+class PreventDeletionError(Exception):
+    pass
+
+
+def check_booking(service: bool = False):
     def decorator(func):
         async def wrapper(callback: CallbackQuery, *args, **kwargs):
-            if date:
-                date_id = int(callback.data.split("_")[2])
-                notes = await GetNotes(date_id=date_id).get_notes()
-                if notes:
-                    user_ids = []
-                    for note in notes:
-                        user_ids.append(note.user_id)
-                    msg = template_manager.get_warning_del_date_or_service(date=True)
-                    keyboard = delete_date_keyboard(date_id, user_ids)
-                    await callback.message.answer(text=msg, reply_markup=keyboard)
-                    await callback.answer()
-                    return
-            elif service:
-                user_id = callback.from_user.id
-                service_id = int(callback.data.split("_")[2])
+            logger.info("Запуск декоратора для перевірки бронювання")
+            id = int(callback.data.split("_")[2])
+            notes = await GetNotes(only_active=True).get_notes()
+            user_ids = ()
+            if notes:
+                if service:
+                    user_ids = tuple(
+                        note.user_id for note in notes if note.service_id == id
+                    )
+                    keyboard = delete_service_keyboard(service_id=id)
+                else:
+                    user_ids = tuple(
+                        note.user_id for note in notes if note.date_id == id
+                    )
+                    keyboard = delete_date_keyboard(date_id=id)
+            elif user_ids:
+                await request_cache.set_request(key="user_ids", value=user_ids)
+                msg = template_manager.get_warning_del_date_or_service()
+                await callback.message.answer(text=msg, reply_markup=keyboard)
+                await callback.answer()
+                raise PreventDeletionError("Існують активні записи")
 
-                notes = await GetNotes(user_id=user_id, only_active=True).get_notes()
-                for note in notes:
-                    user_ids = []
-                    if note.service_id == service_id:
-                        user_ids.append(note.user_id)
-                        msg = template_manager.get_warning_del_date_or_service()
-                        keyboard = delete_service_keyboard(user_ids, service_id)
-                        await callback.message.answer(text=msg, reply_markup=keyboard)
-                        await callback.answer()
-                        return
-            return await func(callback, *args, **kwargs)
+            return await func(callback, id, *args, **kwargs)
 
         return wrapper
 
