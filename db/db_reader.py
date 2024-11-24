73308a069c15f5e64f76e7f3495224b1ce38c6f9
Moses-93 <moieseenko2001@gmail.com>
2024-11-24 13:31:31 +0200

    Rewritten classes for receiving reservations, services, and dates for more flexible database queries, easier scaling, and improved code readability

--- db/db_reader.py
+++ db/db_reader.py
@@ -1,113 +1,49 @@
-from datetime import datetime
-import logging
 from .repository import (
-    NotesRepository,
-    ServiceRepository,
-    FreeDateRepository,
+    BaseGetNotes,
+    BaseGetServices,
+    BaseGetDates,
+    base_get_notes,
+    base_get_service,
+    base_get_free_date,
 )
-
-logger = logging.getLogger(__name__)
-
-
-class GetService:
-    def __init__(self, service_id=None, name=None, all_services=False):
-        self.service_id = service_id
-        self.name = name
-        self.all_services = all_services
-        self.services = None
-
-    async def initialize(self):
-        if self.service_id:
-            self.services = await ServiceRepository.get_service_by_id(self.service_id)
-        elif self.name:
-            self.services = await ServiceRepository.get_service_by_name(self.name)
-        elif self.all_services:
-            self.services = await ServiceRepository.get_all_services()
-
-    async def get(self):
-        await self.initialize()
-        return self.services
-
-    async def get_name(self):
-        await self.initialize()
-        return self.services.name if self.services else None
-
-
-class GetFreeDate:
-    def __init__(self, date_id=None, date=None, free_dates=False, all_dates=False):
-        self.date_id = date_id
-        self.date = date
-        self.free_dates = free_dates
-        self.all_dates = all_dates
-        self.now = datetime.now()
-        self.free_date = None
-        logger.info(f"NOW DATE(in GetFreeDate): {self.now}")
-
-    async def initialize(self):
-        if self.date_id:
-            self.free_date = await FreeDateRepository().get_free_dates_by_date_id(
-                self.date_id
-            )
-        elif self.date:
-            self.free_date = await FreeDateRepository().get_free_date_by_date(self.date)
-        elif self.free_dates:
-            self.free_date = await FreeDateRepository().get_all_free_dates(self.now)
-        elif self.all_dates:
-            self.free_date = await FreeDateRepository().get_dates_last_30_days(self.now)
-        else:
-            self.free_date = None
-
-    async def get(self):
-        await self.initialize()
-        return self.free_date
-
-    @property
-    async def get_date(self):
-        await self.initialize()
-        return self.free_date.date if self.free_date else None
 
 
 class GetNotes:
-    def __init__(
-        self,
-        user_id: int = None,
-        date_id: int = None,
-        note_id: int = None,
-        service_id: int = None,
-        day_filter: int = None,
-        only_active=False,
-    ):
-        self.user_id = user_id
-        self.date_id = date_id
-        self.note_id = note_id
-        self.service_id = service_id
-        self.day_filter = day_filter
-        self.only_active = only_active
 
-    async def initialize(self):
-        now = datetime.now()
-        if self.user_id:
-            if self.only_active:
-                self.notes = await NotesRepository().get_active_notes_by_user_id(
-                    self.user_id, now
-                )
-            else:
-                self.notes = await NotesRepository().get_notes_by_user_id(self.user_id)
-        elif self.date_id:
-            self.notes = await NotesRepository().get_notes_by_date_id(self.date_id)
-        elif self.note_id:
-            self.notes = await NotesRepository().get_active_notes_by_note_id(
-                self.note_id, now
-            )
-        elif self.service_id:
-            self.notes = await NotesRepository().get_notes_by_service_id(
-                self.service_id
-            )
-        elif self.only_active:
-            self.notes = await NotesRepository().get_all_active_notes(now)
-        elif self.day_filter:
-            self.notes = await NotesRepository().get_notes_by_days(self.day_filter)
+    def __init__(self, notes: BaseGetNotes):
+        self.notes = notes
 
-    async def get_notes(self):
-        await self.initialize()
-        return self.notes
+    async def get_notes(self, first=False, **filters):
+        result = await self.notes.get_notes(**filters)
+        if first:
+            return result[0]
+        return result
+
+
+class GetServices:
+
+    def __init__(self, services: BaseGetServices):
+        self.services = services
+
+    async def get_service(self, first=False, **filters):
+        result = await self.services.get_service(**filters)
+        if first:
+            return result[0]
+        return result
+
+
+class GetDates:
+
+    def __init__(self, dates: BaseGetDates):
+        self.dates = dates
+
+    async def get_date(self, first=False, **filters):
+        result = await self.dates.get_date(**filters)
+        if first:
+            return result[0]
+        return result
+
+
+get_notes = GetNotes(base_get_notes)
+get_service = GetServices(base_get_service)
+get_date = GetDates(base_get_free_date)
--- db/repository.py
+++ db/repository.py
@@ -1,190 +1,94 @@
 import logging
-from datetime import datetime, timedelta
-from sqlalchemy import and_, or_, select
-from .models import Service, FreeDate, Notes
-from .config import async_session
+from datetime import datetime
+from .models import Services, Dates, Notes
+from .config import async_session, AsyncSession
 from sqlalchemy.orm import selectinload
-from sqlalchemy.orm import joinedload
+from sqlalchemy import select, update
+from .interfaces import GetNotesInterface, GetDatesInterface, GetServicesInterface
+
 
 logger = logging.getLogger(__name__)
 
 
-class ServiceRepository:
+class BaseGetNotes(GetNotesInterface):
 
-    async def get_filtered_services(
-        session, service_id: int = None, service_name: str = None
-    ):
-        query = select(Service)
-        if service_id:
-            query = query.filter(Service.id == service_id)
-        if service_name:
-            query = query.filter(Service.name == service_name)
-        result = await session.execute(query)
-        return result.scalar()
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
 
-    @staticmethod
-    async def get_service_by_id(service_id: int):
-        async with async_session() as session:
-            return await ServiceRepository.get_filtered_services(session, service_id)
-
-    @staticmethod
-    async def get_service_by_name(name: str):
-        async with async_session() as session:
-            return await ServiceRepository.get_filtered_services(
-                session, service_name=name
+    async def deactivate_old_notes(self, session: AsyncSession):
+        logger.info("Запуск методу для деактивації старих записів")
+        now = datetime.now()
+        stmt = (
+            update(Notes)
+            .where(
+                Notes.active == True,
+                Notes.time < now.time(),
+                Notes.free_date.has(Dates.date <= now.date()),
             )
+            .values(active=False)
+        )
+        await session.execute(stmt)
+        await session.commit()
 
-    @staticmethod
-    async def get_all_services():
-        async with async_session() as session:
-            result = await session.execute(select(Service))
-            return result.scalars().all()
-
-
-class FreeDateRepository:
-
-    @staticmethod
-    async def get_filtered_dates(session, **filters: int | datetime | str | bool):
-        query = select(FreeDate)
-        if filters.get("date_id"):
-            query = query.filter(FreeDate.id == filters.get("date_id"))
-        if filters.get("date"):
-            query = query.filter(FreeDate.date == filters.get("date"))
-        if filters.get("free"):
-            now = filters.get("now")
-            query = query.filter(FreeDate.free.is_(True), FreeDate.now > now)
-        result = await session.execute(query)
-        return result.scalars().all() if filters.get("free") else result.scalar()
-
-    @staticmethod
-    async def get_free_dates_by_date_id(date_id):
-        async with async_session() as session:
-            return await FreeDateRepository.get_filtered_dates(session, date_id=date_id)
-
-    @staticmethod
-    async def get_free_date_by_date(date: datetime):
-        async with async_session() as session:
-            return await FreeDateRepository.get_filtered_dates(session, date=date)
-
-    @staticmethod
-    async def get_all_free_dates(now: datetime):
-        async with async_session() as session:
-            return await FreeDateRepository.get_filtered_dates(
-                session, free=True, now=now
+    async def get_notes(self, **filters):
+        logger.info("Запуск методу для отримання записів")
+        async with self.session() as session:
+            if filters.get("active"):
+                await self.deactivate_old_notes(session)
+            query = select(Notes).options(
+                selectinload(Notes.service), selectinload(Notes.free_date)
             )
-
-    @staticmethod
-    async def get_dates_last_30_days(now: datetime):
-        async with async_session() as session:
-            query = select(FreeDate).filter(FreeDate.date >= now - timedelta(days=30))
+            if filters:
+                query = query.filter_by(**filters)
             result = await session.execute(query)
             return result.scalars().all()
 
 
-class NotesRepository:
+class BaseGetServices(GetServicesInterface):
 
-    @staticmethod
-    async def get_filtered_notes(session, user_id=None, date_id=None, service_id=None):
-        query = select(Notes).options(
-            selectinload(Notes.service), selectinload(Notes.free_date)
-        )
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
 
-        if user_id:
-            query = query.filter(Notes.user_id == user_id)
-        if date_id:
-            query = query.filter(Notes.date_id == date_id)
-        if service_id:
-            query = query.filter(Notes.service_id == service_id)
-
-        result = await session.execute(query)
-        return result.scalars().all()
-
-    @staticmethod
-    async def get_notes_by_user_id(user_id: int):
-        async with async_session() as session:
-            return await NotesRepository.get_filtered_notes(session, user_id=user_id)
-
-    @staticmethod
-    async def get_notes_by_date_id(date_id: int):
-        async with async_session() as session:
-            return await NotesRepository.get_filtered_notes(session, date_id=date_id)
-
-    @staticmethod
-    async def get_notes_by_service_id(service_id: int):
-        async with async_session() as session:
-            return await NotesRepository.get_filtered_notes(
-                session, service_id=service_id
-            )
-
-    @staticmethod
-    async def get_filtered_active_notes(
-        session, now: datetime, user_id: int = None, note_id: int = None
-    ):
-        # Створюємо запит з жадним завантаженням (joinedload) для пов'язаних моделей
-        query = (
-            select(Notes)
-            .join(FreeDate, FreeDate.id == Notes.date_id)
-            .options(
-                joinedload(Notes.service),  # Жадне завантаження для зв'язку з Service
-                joinedload(
-                    Notes.free_date
-                ),  # Жадне завантаження для зв'язку з FreeDate
-            )
-            .filter(
-                or_(
-                    FreeDate.date > now.date(),
-                    and_(
-                        FreeDate.date == now.date(),
-                        Notes.time > now.time(),
-                    ),
-                )
-            )
-        )
-
-        # Додаємо фільтрацію за user_id, якщо задано
-        if user_id:
-            query = query.filter(Notes.user_id == user_id)
-
-        # Додаємо фільтрацію за note_id, якщо задано
-        if note_id:
-            query = query.filter(Notes.id == note_id)
-
-        # Виконуємо запит і повертаємо результат
-        result = await session.execute(query)
-        return result.scalars().all()
-
-    @staticmethod
-    async def get_notes_by_days(day: int):
-        async with async_session() as session:
-            # Визначаємо стартову дату фільтрації
-            start_time = datetime.now() - timedelta(days=day)
-            # Створюємо запит з жадним завантаженням
-            query = (
-                select(Notes)
-                .join(FreeDate, FreeDate.id == Notes.date_id)
-                .options(
-                    joinedload(Notes.free_date), joinedload(Notes.service)
-                )  # Жадне завантаження
-                .filter(FreeDate.date >= start_time.date())
-            )
+    async def get_service(self, **filters):
+        logger.info("Запуск методу для отримання послуг")
+        async with self.session() as session:
+            query = select(Services)
+            if filters:
+                query = query.filter_by(**filters)
             result = await session.execute(query)
             return result.scalars().all()
 
-    @staticmethod
-    async def get_all_active_notes(now: datetime):
-        async with async_session() as session:  # Використовуйте асинхронну сесію
-            return await NotesRepository.get_filtered_active_notes(session, now)
 
-    @staticmethod
-    async def get_active_notes_by_user_id(user_id: int, now: datetime):
-        async with async_session() as session:
-            return await NotesRepository.get_filtered_active_notes(
-                session, now, user_id=user_id
-            )
+class BaseGetDates(GetDatesInterface):
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
 
-    @staticmethod
-    async def get_active_notes_by_note_id(note_id: int, now: datetime):
-        async with async_session() as session:
-            return await NotesRepository.get_filtered_active_notes(
-                session, now, note_id=note_id
+    async def deactivate_old_date(self, session: AsyncSession):
+        logger.info("Запуск методу для деактивації старих дат")
+        now = datetime.now()
+        stmt = (
+            update(Dates)
+            .where(
+                Dates.free == True,
+                Dates.now < now,
             )
+            .values(free=False)
+        )
+        await session.execute(stmt)
+        await session.commit()
+
+    async def get_date(self, **filters):
+        logger.info("Запуск методу для отримання дат")
+        async with self.session() as session:
+            if filters.get("free"):
+                await self.deactivate_old_date(session)
+            query = select(Dates)
+            if filters:
+                query = query.filter_by(**filters)
+            result = await session.execute(query)
+            return result.scalars().all()
+
+
+base_get_free_date = BaseGetDates(async_session)
+base_get_service = BaseGetServices(async_session)
+base_get_notes = BaseGetNotes(async_session)
