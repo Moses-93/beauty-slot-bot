eb3be5080a70b37dc07448c1eec7a417ae29cbab
Moses-93 <moieseenko2001@gmail.com>
2024-12-12 10:57:45 +0200

    Import of admin_keyboards changed to general_keyboards

--- tests/test_admin_handlers.py
+++ tests/test_admin_handlers.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import AsyncMock, patch, MagicMock
 from datetime import datetime, timedelta
 from aiogram import types
-from bot.admin.keyboards import admin_keyboards
+from bot.admin.keyboards import general_keyboards
 from bot.admin.handlers import (
     general_handlers,
     dates_handlers,
@@ -16,7 +16,7 @@ async def test_show_admin_panel_is_admin():
     message = AsyncMock(spec=types.Message)
     message.answer = AsyncMock()
     keyboard = AsyncMock(spec=types.ReplyKeyboardMarkup)
-    admin_keyboards.main_keyboard = keyboard
+    general_keyboards.main_keyboard = keyboard
     await general_handlers.show_admin_panel(message, is_admin=True)
 
     message.answer.assert_called_once_with(
@@ -29,6 +29,6 @@ async def test_show_admin_panel_not_admin():
     message = AsyncMock(spec=types.Message)
     message.answer = AsyncMock()
     keyboard = AsyncMock(spec=types.ReplyKeyboardMarkup)
-    admin_keyboards.main_keyboard = keyboard
+    general_keyboards.main_keyboard = keyboard
     await general_handlers.show_admin_panel(message, is_admin=False)
     message.answer.assert_called_once_with(text="У Вас немає доступу до адмін панелі.")
