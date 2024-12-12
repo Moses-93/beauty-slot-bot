bd6958d8b7205563d39f0f26e4d30348f778da36
Moses-93 <moieseenko2001@gmail.com>
2024-12-12 10:37:29 +0200

    Added a query to the database to check for an admin

--- bot/admin/middleware.py
+++ bot/admin/middleware.py
@@ -1,13 +1,17 @@
+import logging
 from aiogram import BaseMiddleware
+from db.crud import admins_manager
+
+logger = logging.getLogger(__name__)
 
 
 class AdminMiddleware(BaseMiddleware):
 
-    def __init__(self, admin_ids: list):
+    def __init__(self):
         super().__init__()
-        self.admin_ids = admin_ids
 
     async def __call__(self, handler, event, data: dict):
         user_id = event.from_user.id
-        data["is_admin"] = user_id in self.admin_ids
+        admin = await admins_manager.read(chat_id=user_id)
+        data["is_admin"] = True if admin else False
         return await handler(event, data)
