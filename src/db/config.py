868a489720fc9d162d5d9d9250fca7b939633025
Moses-93 <moieseenko2001@gmail.com>
2025-04-29 16:43:32 +0300

    refactor: Update database connection configuration to use environment variables

--- src/db/config.py
+++ src/db/config.py
@@ -3,6 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from os import getenv
 
 load_dotenv()
-URI = getenv("URI")
-engine = create_async_engine(URI)
+
+PG_USER = getenv("POSTGRES_USER")
+PG_PASSWORD = getenv("POSTGRES_PASSWORD")
+PG_DB = getenv("POSTGRES_DB")
+PG_HOST = getenv("POSTGRES_HOST")
+PG_PORT = getenv("POSTGRES_PORT")
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+)
+
+engine = create_async_engine(DATABASE_URL)
 Session = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
