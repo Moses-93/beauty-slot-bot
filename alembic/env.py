8f1645b7d34424dbc7095556b70a95e7cd0ed39a
Moses-93 <moieseenko2001@gmail.com>
2025-04-29 16:43:06 +0300

    refactor: Update model import and target metadata for Alembic configuration

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 from os import getenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from db.models import Notes
+from src.db.models import Base
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -23,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Notes.metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
