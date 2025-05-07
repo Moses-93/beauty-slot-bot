e03411f55c7ba5eeb96e0e6506a447e8d81a0368
Moses-93 <moieseenko2001@gmail.com>
2025-05-07 10:26:40 +0300

    chore: Update AbstractDateRepository and AbstractServiceRepository methods to be asynchronous

--- src/domain/repositories/abstract_date_repository.py
+++ src/domain/repositories/abstract_date_repository.py
@@ -6,26 +6,26 @@ from src.domain.entities.date import Date
 
 class AbstractDateRepository(ABC):
     @abstractmethod
-    def get_dates(self) -> List[Date]:
+    async def get_dates(self) -> List[Date]:
         """Get all dates."""
         pass
 
     @abstractmethod
-    def get_date_by_id(self, date_id: int) -> Date:
+    async def get_date_by_id(self, date_id: int) -> Date:
         """Get a date by its ID."""
         pass
 
     @abstractmethod
-    def create(self, date_data: Date) -> Date:
+    async def create(self, date_data: Date) -> Date:
         """Create a new date."""
         pass
 
     @abstractmethod
-    def update(self, date_id: int, date: Date) -> Date:
+    async def update(self, date_id: int, date: Date) -> Date:
         """Update an existing date."""
         pass
 
     @abstractmethod
-    def delete(self, date_id: int) -> None:
+    async def delete(self, date_id: int) -> None:
         """Delete a date by its ID."""
         pass
--- src/domain/repositories/abstract_service_repository.py
+++ src/domain/repositories/abstract_service_repository.py
@@ -6,26 +6,26 @@ from src.domain.entities.service import Service
 
 class AbstractServiceRepository(ABC):
     @abstractmethod
-    def get_services(self) -> List[Service]:
+    async def get_services(self) -> List[Service]:
         """Get all services."""
         pass
 
     @abstractmethod
-    def get_service_by_id(self, service_id: int) -> Service:
+    async def get_service_by_id(self, service_id: int) -> Service:
         """Get a service by its ID."""
         pass
 
     @abstractmethod
-    def create(self, service_data: Service) -> Service:
+    async def create(self, service_data: Service) -> Service:
         """Create a new service."""
         pass
 
     @abstractmethod
-    def update(self, service_id: int, service: Service) -> Service:
+    async def update(self, service_id: int, service: Service) -> Service:
         """Update an existing service."""
         pass
 
     @abstractmethod
-    def delete(self, service_id: int) -> None:
+    async def delete(self, service_id: int) -> None:
         """Delete a service by its ID."""
         pass
