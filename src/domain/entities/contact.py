ce56b67ac937e436822ddb8bef330c7e7966f4ba
Moses-93 <moieseenko2001@gmail.com>
2025-05-07 10:24:09 +0300

    chore: Add work_start_time and work_end_time fields to Contact class

--- src/domain/entities/contact.py
+++ src/domain/entities/contact.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Optional
+from datetime import time
 
 
 @dataclass
@@ -14,3 +15,5 @@ class Contact:
     instagram_link: Optional[str] = field(default=None)
     google_maps_link: Optional[str] = field(default=None)
     about: Optional[str] = field(default=None)
+    work_start_time: time = field(default=None)
+    work_end_time: time = field(default=None)
