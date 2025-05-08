fe02c730603ea21929a46433312a696407425fc8
Moses-93 <moieseenko2001@gmail.com>
2025-05-08 15:32:37 +0300

    feat: Implement CheckBookingAvailabilityUseCase for checking booking availability

---
+++ src/application/use_cases/time_checker.py
@@ -0,0 +1,31 @@
+from datetime import time, timedelta
+from src.domain.repositories.abstract_booking_repository import (
+    AbstractBookingRepository,
+)
+from src.domain.entities.time import TimeSlot
+from src.application.dto import TimeCheckResultDTO
+from src.application.services.time_finder import AvailableTimeFinder
+
+
+class CheckBookingAvailabilityUseCase:
+    def __init__(
+        self, booking_repo: AbstractBookingRepository, time_finder: AvailableTimeFinder
+    ):
+        self._booking_repo = booking_repo
+        self._time_finder = time_finder
+
+    async def check_time(
+        self, date_id: int, requested_time: time, service_duration: timedelta
+    ) -> TimeCheckResultDTO:
+        busy_slots = await self._booking_repo.get_busy_slots(date_id)
+        user_time_slot = TimeSlot.create_with_duration(requested_time, service_duration)
+        is_available = not any(
+            busy_slot.overlaps(user_time_slot) for busy_slot in busy_slots
+        )
+        if is_available:
+            return TimeCheckResultDTO(is_available=True)
+        else:
+            nearby_slots = await self._time_finder.find_nearby(
+                requested_time, busy_slots, service_duration
+            )
+            return TimeCheckResultDTO(is_available=False, time_offers=nearby_slots)
