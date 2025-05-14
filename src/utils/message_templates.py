873459f2ec091abdc85412120506cd9233f78728
Moses-93 <moieseenko2001@gmail.com>
2025-05-14 10:38:49 +0300

    refactor: Move booking notifications to a separate module

--- src/utils/message_templates.py
+++ src/utils/message_templates.py
@@ -1,8 +1,3 @@
-from datetime import datetime, time
-from src.db.models import Booking
-from src.cache.cache import request_cache, user_cache
-
-
 class TemplateManager:
 
     @staticmethod
@@ -16,19 +11,8 @@ class TemplateManager:
         return message
 
     @staticmethod
-    async def get_booking_confirmation(user_id: int, time: time):
-        service, date = await user_cache.get_user_cache(user_id, "service", "date")
-        message = (
-            f"Ви щойно здійснили запис на послугу - {service.name}\n"
-            f"Чекаю Вас {date.date} o {time}.\n"
-            f"Будь ласка, зв'яжіться зі мною, коли будете знаходитись за моєю адресою. Я Вас зустріну\n"
-            "Якщо Ви розумієте, що не прийдете або запізнюєтесь - будь ласка, скасуйте запис або повідомте про це мене"
-        )
-        return message
-
-    @staticmethod
     async def message_to_the_master(
-        username: str, service: str, date: datetime, time, booking_cancel=False
+        username: str, service: str, date, time, booking_cancel=False
     ):
         message = (
             f"{"Скасований запис:\n" if booking_cancel else "Новий запис:\n"}"
@@ -122,74 +106,12 @@ class TemplateManager:
             return message
 
     @staticmethod
-    def get_cancel_notification():
-        message = f"Запис скасовано"
-        return message
-
-    @staticmethod
-    def elapsed_time_warning(time: datetime):
+    def elapsed_time_warning(time):
         message = f"Ви не можете обрати {time.time()}, оскільки він вже пройшов"
         return message
 
-    @staticmethod
-    def service_selection_info():
-        message = "Оберіть дату, на яку бажаєте записатись"
-        return message
-
-    @staticmethod
-    def busy_time_notification(time):
-        message = f"На жаль, вказаний вами час зайнятий. Найближчий доступний час: {time}.\
-        \nОберіть цей час, або вкажіть інший"
-        return message
 
     @staticmethod
     def get_greeting_message():
         message = "Чим можу бути корисний?"
         return message
-
-    @staticmethod
-    def get_service_options():
-        message = "Оберіть бажану послугу:"
-        return message
-
-    @staticmethod
-    def get_entry_options():
-        message = "Оберіть потрібні Вам записи:"
-        return message
-
-    @staticmethod
-    def booking_not_found():
-        message = "На жаль, я не зміг знайти жодних записів у обраній категорії("
-        return message
-
-    @staticmethod
-    def date_selection_prompt():
-        message = f"Напишіть час до 18:00 у форматі 'ГГ:ХХ'"
-        return message
-
-    @staticmethod
-    async def successful_booking_notification(user_id: int, time: time):
-        service, date = await user_cache.get_user_cache(user_id, "service", "date")
-        message = f"Ви успішно записались на послугу - {service.name}. \nЧекаю на Вас {date.date} о {time}"
-        return message
-
-    @staticmethod
-    def get_reminder(choice=False, hour=None, note: Booking = None):
-        if choice:
-            message = "Оберіть за скільки годин бажаєте отримати нагадування:"
-            return message
-        if note:
-            message = (
-                f"Нагадуємо, Ви записані на послугу - {note.service.name}\n"
-                f"Дата: {note.date.date}\n"
-                f"Час: {note.time}\n"
-                "Адреса: вул. Перлинна 3. ЖК 5 Перлина\n"
-            )
-            return message
-
-        else:
-            message = f"Нагадування буде надіслано за {hour} год. до запису."
-            return message
-
-
-template_manager = TemplateManager()
