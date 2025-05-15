033b360a2ddb49c73cf0aa0047a00a9f17edaabb
Moses-93 <moieseenko2001@gmail.com>
2025-05-15 10:45:20 +0300

    chore: Add MasterKeyboard class to implement keyboard layout for bot

---
+++ src/bot/keyboard/master.py
@@ -0,0 +1,61 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+)
+
+
+class MasterKeyboard:
+
+    @staticmethod
+    def main_section() -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="📒 Записи")],
+                [
+                    KeyboardButton(text="📖 Послуги"),
+                    KeyboardButton(text="📅 Дати"),
+                ],
+            ],
+            resize_keyboard=True,
+        )
+
+    @staticmethod
+    def services_section() -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="➕ Додати послугу"),
+                    KeyboardButton(text="➖ Видалити послугу"),
+                ],
+                [KeyboardButton(text="📋 Список послуг")],
+                [KeyboardButton(text="🔙 Назад")],
+            ],
+            resize_keyboard=True,
+        )
+
+    @staticmethod
+    def dates_section() -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="➕ Додати дату"),
+                    KeyboardButton(text="➖ Видалити дату"),
+                ],
+                [KeyboardButton(text="📅 Список дат")],
+                [KeyboardButton(text="🔙 Назад")],
+            ],
+            resize_keyboard=True,
+        )
+
+    @staticmethod
+    def bookings_section() -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Активні записи"),
+                    KeyboardButton(text="Всі записи"),
+                ],
+                [KeyboardButton(text="🔙 Назад")],
+            ],
+            resize_keyboard=True,
+        )
