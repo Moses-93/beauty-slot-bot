25ec72587cb59127887dc8c84f61ed96a140596c
Moses-93 <moieseenko2001@gmail.com>
2025-05-15 10:49:09 +0300

    refactor: Simplify handlers. Move functions to the class body.

--- src/bot/master/handlers/general_handlers.py
+++ src/bot/master/handlers/general_handlers.py
@@ -1,66 +1,34 @@
 import logging
-
-from aiogram import F, Router
 from aiogram.types import Message
 
-from ..middleware import AdminMiddleware
-from ..keyboards import (
-    date_keyboard,
-    general_keyboards,
-    service_keybord,
-    show_booking_keyboards,
-)
-from src.decorators.permissions import admin_only
+from src.bot.keyboard.master import MasterKeyboard
+from src.bot.messages.master import MasterMessages
 
 logger = logging.getLogger(__name__)
 
-router = Router()
-router.message.middleware(AdminMiddleware())
-router.callback_query.middleware(AdminMiddleware())
 
+class MenuHandler:
 
-@router.message(F.text == "admin")
-async def show_admin_panel(message: Message, is_admin: bool, *args, **kwargs):
-    logger.info(f"USER ID(show_admin_panel): {is_admin}")
-    if is_admin:
-        msg = "Ви перейшли в панель адміністратора"
-        keyboard = general_keyboards.main_keyboard
-        await message.answer(text=msg, reply_markup=keyboard)
-        return
-    else:
-        await message.answer(text="У Вас немає доступу до адмін панелі.")
-        return
+    async def show_main_menu(self, message: Message):
+        await message.answer(
+            text=MasterMessages.start(),
+            reply_markup=MasterKeyboard.main_section(),
+        )
 
+    async def show_services_menu(self, message: Message):
+        await message.answer(
+            text=MasterMessages.services_menu(),
+            reply_markup=MasterKeyboard.services_section(),
+        )
 
-@router.message(F.text == "Керування послугами")
-@admin_only
-async def manage_services(message: Message, *args, **kwargs):
-    msg = "Оберіть дію:"
-    keyboard = service_keybord.manage_service_keyboard
-    await message.answer(text=msg, reply_markup=keyboard)
-    return
+    async def show_dates_menu(self, message: Message):
+        await message.answer(
+            text=MasterMessages.dates_menu(),
+            reply_markup=MasterKeyboard.dates_section(),
+        )
 
-
-@router.message(F.text == "Керування датами")
-@admin_only
-async def manage_dates(message: Message, *args, **kwargs):
-    msg = "Оберіть дію:"
-    keyboard = date_keyboard.manage_dates_keyboard
-    await message.answer(text=msg, reply_markup=keyboard)
-
-
-@router.message(F.text == "Керування адміністраторами")
-@admin_only
-async def manage_admins(message: Message, *args, **kwargs):
-    msg = "Оберіть дію:"
-    keyboard = general_keyboards.manage_admins_keyboard
-    await message.answer(text=msg, reply_markup=keyboard)
-    return
-
-
-@router.message(F.text == "Записи")
-@admin_only
-async def show_all_bookings(message: Message, *args, **kwargs):
-    msg = "Оберіть дію:"
-    keyboard = show_booking_keyboards.show_bookings_keyboard
-    await message.answer(text=msg, reply_markup=keyboard)
+    async def show_bookings_menu(self, message: Message):
+        await message.answer(
+            text=MasterMessages.bookings_menu(),
+            reply_markup=MasterKeyboard.bookings_section(),
+        )
