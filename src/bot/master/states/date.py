8f3317f6b08206cec46b4e5f40100040843af3f5
Moses-93 <moieseenko2001@gmail.com>
2025-05-15 17:13:05 +0300

    refactor: Rename and separate date states into distinct classes for deletion and creation

--- src/bot/master/states/date.py
+++ src/bot/master/states/date.py
@@ -1,7 +1,15 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class DateStates(StatesGroup):
+class DeleteDateStates(StatesGroup):
+    """
+    This class defines states for the date deletion process
+    """
+
+    date_id = State()
+
+
+class CreateDateStates(StatesGroup):
     """
     This class defines states for the date creation process
     """
