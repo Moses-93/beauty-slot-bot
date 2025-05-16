5b70fcd7ce8230cc4fca9bd47c49f0a97d5cd9f0
Moses-93 <moieseenko2001@gmail.com>
2025-05-16 14:37:21 +0300

    refactor: Update AbstractDateRepository to use DateDTO instead of Date entities

--- src/domain/repositories/abstract_date_repository.py
+++ src/domain/repositories/abstract_date_repository.py
@@ -1,27 +1,27 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from src.domain.entities.date import Date
+from src.application.dto.date import DateDTO
 
 
 class AbstractDateRepository(ABC):
     @abstractmethod
-    async def get_dates(self) -> List[Date]:
+    async def get_dates(self) -> List[DateDTO]:
         """Get all dates."""
         pass
 
     @abstractmethod
-    async def get_date_by_id(self, date_id: int) -> Date:
+    async def get_date_by_id(self, date_id: int) -> DateDTO:
         """Get a date by its ID."""
         pass
 
     @abstractmethod
-    async def create(self, date_data: Date) -> Date:
+    async def create(self, date_data: DateDTO) -> DateDTO:
         """Create a new date."""
         pass
 
     @abstractmethod
-    async def update(self, date_id: int, date: Date) -> Date:
+    async def update(self, date_id: int, date: DateDTO) -> DateDTO:
         """Update an existing date."""
         pass
 
