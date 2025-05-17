eb9b6883a9a20b8c1072bf19279a45e9b56b7b75
Moses-93 <moieseenko2001@gmail.com>
2025-05-17 13:12:01 +0300

    refactor: Set the default value for data in ResultDTO to None and add methods for easy use

--- src/shared/dto/result.py
+++ src/shared/dto/result.py
@@ -7,4 +7,13 @@ T = TypeVar("T")
 @dataclass
 class ResultDTO(Generic[T]):
     is_success: bool
-    data: Optional[T]
+    data: Optional[T] = None
+
+    @classmethod
+    def success(cls, data: T) -> "ResultDTO[T]":
+        return cls(is_success=True, data=data)
+
+    @classmethod
+    def fail(cls) -> "ResultDTO[None]":
+        return cls(is_success=False)
+
