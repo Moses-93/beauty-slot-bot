e45b553c91e6b39a60ef85856da1a37df368ff38
Moses-93 <moieseenko2001@gmail.com>
2025-06-01 18:23:29 +0300

    feat: Implement the navigation class handler

---
+++ src/bot/master/handlers/navigation.py
@@ -0,0 +1,33 @@
+import logging
+from punq import Container
+from aiogram.types import Message
+
+from src.bot.master.keyboard.section import SectionKeyboards
+from src.bot.master.messages.section import SectionMessages
+
+
+class SectionHandler:
+    def __init__(self, container: Container):
+        self.container = container
+
+    async def show_sections(self, message: Message):
+        await message.answer(
+            text=SectionMessages.start(), reply_markup=SectionKeyboards.main()
+        )
+
+    async def show_dates_section(self, message: Message):
+        await message.answer(
+            text=SectionMessages.dates(), reply_markup=SectionKeyboards.dates()
+        )
+
+    async def show_services_section(self, message: Message):
+        await message.answer(
+            text=SectionMessages.services(),
+            reply_markup=SectionKeyboards.services(),
+        )
+
+    async def show_booking_section(self, message: Message):
+        await message.answer(
+            text=SectionMessages.bookings(),
+            reply_markup=SectionKeyboards.bookings(),
+        )
