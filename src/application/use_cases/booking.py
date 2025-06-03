a5c3f7d689da26c6a14bea21b2272c7e4060c747
Moses-93 <moieseenko2001@gmail.com>
2025-06-03 09:44:22 +0300

    chore: Add scheduling for booking deactivation in CreateBookingUseCase

--- src/application/use_cases/booking.py
+++ src/application/use_cases/booking.py
@@ -1,14 +1,26 @@
+from datetime import date, time, datetime
 from src.domain.entities.booking import Booking
 from src.domain.repositories.abstract_booking_repository import (
     AbstractBookingRepository,
 )
 from src.shared.dto.result import ResultDTO
 from src.application.dto.booking import BookingDTO
+from src.infrastructure.celery.tasks.deactivation.booking import deactivate
 
 
 class CreateBookingUseCase:
     def __init__(self, booking_repo: AbstractBookingRepository):
         self._booking_repo = booking_repo
+
+    async def _schedule_deactivation(booking_id: int, date: date, time: time) -> None:
+        """
+        Schedule a deactivation task for the booking.
+        """
+        deactivation_time = datetime.combine(date, time)
+        await deactivate.apply_async(
+            args=[booking_id],
+            eta=deactivation_time,
+        )
 
     async def __call__(
         self, booking_dto: BookingDTO, *args, **kwds
@@ -21,6 +33,11 @@ class CreateBookingUseCase:
         created_booking = await self._booking_repo.create(booking)
 
         if created_booking:
+            await self._schedule_deactivation(
+                created_booking.id,
+                created_booking.date.date,
+                created_booking.time,
+            )
             return ResultDTO.success(created_booking)
 
         return ResultDTO.fail()
