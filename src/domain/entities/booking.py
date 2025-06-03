b834bbac9bc4436220622945cf0990067ede441f
Moses-93 <moieseenko2001@gmail.com>
2025-06-03 09:41:29 +0300

    refactor: Update Booking class to use DTOs and remove to_dict method

--- src/domain/entities/booking.py
+++ src/domain/entities/booking.py
@@ -1,26 +1,16 @@
 from dataclasses import dataclass, field
 from datetime import time as Time, datetime
-from typing import Dict, Optional
+from typing import Optional
+from src.application.dto.date import DateDTO
+from src.application.dto.service import ServiceDTO
 
 
 @dataclass
 class Booking:
     id: Optional[int] = field(default=None)
     user_id: int = field(default=None)
-    service_id: int = field(default=None)
-    date_id: int = field(default=None)
+    service: ServiceDTO = field(default=None)
+    date: DateDTO = field(default=None)
     time: Time = field(default=None)
     reminder_time: Optional[datetime] = field(default=None)
     is_active: bool = field(default=True)
-
-    def to_dict(self) -> Dict:
-        """Convert the Booking object to a dictionary."""
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "service_id": self.service_id,
-            "date_id": self.date_id,
-            "time": self.time,
-            "reminder_time": self.reminder_time,
-            "is_active": self.is_active,
-        }
