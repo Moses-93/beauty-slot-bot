403cd00921e3aa8811f65067c8f71d43c48679b2
Moses-93 <moieseenko2001@gmail.com>
2025-06-09 14:30:40 +0300

    refactor: Remove UserIDMiddleware as it is no longer needed

--- src/bot/client/middleware.py
+++
@@ -1,7 +0,0 @@
-from aiogram import BaseMiddleware
-
-
-class UserIDMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event, data: dict):
-        data["user_id"] = event.from_user.id
-        return await handler(event, data)
