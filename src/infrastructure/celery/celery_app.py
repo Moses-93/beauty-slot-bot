3b83113093dcb33bf88765ce199d86a1e416db2d
Moses-93 <moieseenko2001@gmail.com>
2025-06-09 14:11:17 +0300

    refactor: Update Celery app configuration and enable task autodiscovery

--- src/infrastructure/celery/celery_app.py
+++ src/infrastructure/celery/celery_app.py
@@ -1,6 +1,18 @@
+import logging
 from celery import Celery
 
 from .celeryconfig import Config
 
-celery_app = Celery("tasks")
+logger = logging.getLogger(__name__)
+
+celery_app = Celery("beauty_slot")
 celery_app.config_from_object(Config)
+
+logger.info("Autodiscovering Celery tasks...")
+
+celery_app.autodiscover_tasks(
+    [
+        "infrastructure.celery.tasks.deactivation",
+        "infrastructure.celery.tasks.reminders",
+    ]
+)
