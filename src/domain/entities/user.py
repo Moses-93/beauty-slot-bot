37eaec50cd0e6692625d202bb45db7ccbd1efa42
Moses-93 <moieseenko2001@gmail.com>
2025-06-10 18:44:12 +0300

    refactor: Rearrange User class properties for improved readability

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -7,18 +7,21 @@ from src.domain.entities.booking import Booking
 
 @dataclass
 class User:
+    id: Optional[int] = None
     name: str
     username: Optional[str] = None
     chat_id: str
     role: UserRole
     bookings: Optional[List[Booking]] = None
 
+    @property
     def is_master(self) -> bool:
         """
         Check if the user is an admin.
         """
         return self.role == UserRole.MASTER
 
+    @property
     def is_client(self) -> bool:
         """
         Check if the user is a client.
