293ba30ce2aa6f5cb6c1d9c9c5a3b313e5e6829b
Moses-93 <moieseenko2001@gmail.com>
2025-06-16 20:28:28 +0300

    refactor: Rename BOOKINGS to ALL_BOOKINGS and add ACTIVE_BOOKINGS to PaginationCategory

--- src/bot/shared/enums/pagination.py
+++ src/bot/shared/enums/pagination.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class PaginationCategory(str, Enum):
     SERVICES = "services"
-    BOOKINGS = "bookings"
+    ALL_BOOKINGS = "all_bookings"
+    ACTIVE_BOOKINGS = "active_bookings"
     DATES = "dates"
     CONTACTS = "contacts"
