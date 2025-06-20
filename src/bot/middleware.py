a709d6a6e66de5c371f8934e1881f21a71f6d236
Moses-93 <moieseenko2001@gmail.com>
2025-06-20 17:02:31 +0300

    chore: Add middleware setup

---
+++ src/bot/middleware.py
@@ -0,0 +1,14 @@
+from aiogram import Dispatcher
+from punq import Container
+
+from src.bot.shared.middlewares.auth import AttachUserMiddleware
+
+
+def setup_middlewares(dispatcher: Dispatcher, container: Container):
+
+    ensure_user_uc = container.resolve("EnsureUserExistsUseCase")
+
+    attach_user = AttachUserMiddleware(ensure_user_uc)
+
+    dispatcher.message.middleware.register(attach_user)
+    dispatcher.callback_query.middleware.register(attach_user)
