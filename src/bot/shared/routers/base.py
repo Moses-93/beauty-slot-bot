fb53c6c9004dae4e64dd9b8442bcaea5e6127039
Moses-93 <moieseenko2001@gmail.com>
2025-06-20 12:52:10 +0300

    refactor: Remove the property method and make the router attribute is public

--- src/bot/shared/routers/base.py
+++ src/bot/shared/routers/base.py
@@ -3,12 +3,8 @@ from aiogram import Router
 
 class BaseRouter:
     def __init__(self, router: Router):
-        self._router = router
+        self.router = router
         self._register()
-
-    @property
-    def router(self):
-        return self._router
 
     def _register(self):
         raise NotImplementedError("Subclasses should implement this method.")
