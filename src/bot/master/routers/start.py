f7dfebd7aa80ef3a8e51536698e998656e266016
Moses-93 <moieseenko2001@gmail.com>
2025-06-20 12:15:35 +0300

    feat: Implement StartRouter for client and master roles

---
+++ src/bot/master/routers/start.py
@@ -0,0 +1,20 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+
+from src.bot.shared.routers.base import BaseRouter
+from src.bot.master.handlers.start import StartHandler
+from src.bot.shared.filters.user import RoleFilter
+from src.domain.enums.user_role import UserRole
+
+
+class StartRouter(BaseRouter):
+    def __init__(self):
+        super().__init__(router=Router())
+        self._handler = StartHandler()
+
+    def _register(self):
+        self._router.message.register(
+            self._handler.show_sections,
+            CommandStart,
+            RoleFilter(roles={UserRole.MASTER}),
+        )
