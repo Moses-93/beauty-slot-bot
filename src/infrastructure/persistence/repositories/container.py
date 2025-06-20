b574a651f8060ebb869e8bd86f0c0cf954ea3e73
Moses-93 <moieseenko2001@gmail.com>
2025-06-20 18:30:04 +0300

    chore: Register ContactRepository in the container

--- src/infrastructure/persistence/repositories/container.py
+++ src/infrastructure/persistence/repositories/container.py
@@ -4,6 +4,7 @@ from .base_repository import BaseRepository
 from .booking_repository import BookingRepository
 from .date_repository import DateRepository
 from .service_repository import ServiceRepository
+from .contact_repository import ContactRepository
 
 
 def register(container: Container) -> None:
@@ -14,3 +15,4 @@ def register(container: Container) -> None:
     container.register(BookingRepository)
     container.register(ServiceRepository)
     container.register(DateRepository)
+    container.register(ContactRepository)
