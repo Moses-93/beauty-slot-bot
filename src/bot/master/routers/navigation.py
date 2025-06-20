240b838bbdacb2c078d4f339670c258585befee1
Moses-93 <moieseenko2001@gmail.com>
2025-06-20 14:58:23 +0300

    refactor: Simplify SectionRouter by inheriting from BaseRouter and updating initialization

--- src/bot/master/routers/navigation.py
+++ src/bot/master/routers/navigation.py
@@ -1,20 +1,16 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart
+from punq import Container
 
 from src.bot.master.handlers.navigation import SectionHandler
 from src.bot.shared.filters.user import RoleFilter
 from src.domain.enums.user_role import UserRole
+from src.bot.shared.routers.base import BaseRouter
 
 
-class SectionRouter:
-    def __init__(self, handler: SectionHandler):
-        self._router = Router(name="section")
-        self._handler = handler
-        self._register()
-
-    @property
-    def router(self):
-        return self._router
+class SectionRouter(BaseRouter):
+    def __init__(self, container: Container):
+        self._handler = SectionHandler(container)
+        super().__init__(Router(name="section"))
 
     def _register(self) -> None:
         self._router.message.register(self._handler.show_sections, CommandStart)
