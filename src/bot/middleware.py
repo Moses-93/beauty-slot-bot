30f2afc6b5e426f6cee2dbf94d9bad0a83cfe64e
Moses-93 <moieseenko2001@gmail.com>
2025-06-21 12:21:48 +0300

    refactor: Fix EnsureUserExistsUseCase resolution in middleware setup

--- src/bot/middleware.py
+++ src/bot/middleware.py
@@ -1,12 +1,13 @@
 from aiogram import Dispatcher
 from punq import Container
 
+from src.application.use_cases.user import EnsureUserExistsUseCase
 from src.bot.shared.middlewares.auth import AttachUserMiddleware
 
 
 def setup_middlewares(dispatcher: Dispatcher, container: Container):
 
-    ensure_user_uc = container.resolve("EnsureUserExistsUseCase")
+    ensure_user_uc = container.resolve(EnsureUserExistsUseCase)
 
     attach_user = AttachUserMiddleware(ensure_user_uc)
 
