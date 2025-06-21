c5e8a7623ddb09b8bfba35b681ebff3167837ade
Moses-93 <moieseenko2001@gmail.com>
2025-06-21 12:22:46 +0300

    refactor: Update use case and repository registrations in containers

--- src/application/use_cases/container.py
+++ src/application/use_cases/container.py
@@ -1,20 +1,30 @@
 from punq import Container
 
 from . import date, service
-from .booking import CreateBookingUseCase
+from . import booking
+from .user import EnsureUserExistsUseCase
 from .time_checker import CheckBookingAvailabilityUseCase
+from .contact import GetContactsUseCase
 
 
 def register(container: Container) -> None:
     """
     Register all use cases in the container.
     """
+    container.register(EnsureUserExistsUseCase)
     container.register(
         CheckBookingAvailabilityUseCase,
     )
     container.register(
-        CreateBookingUseCase,
+        booking.CreateBookingUseCase,
     )
+    container.register(
+        booking.DeactivateBookingUseCase,
+    )
+    container.register(
+        booking.GetBookingUseCase,
+    )
+
     container.register(date.CreateDateUseCase)
     container.register(date.DeactivateDateUseCase)
     container.register(date.DeleteDateUseCase)
@@ -22,3 +32,4 @@ def register(container: Container) -> None:
     container.register(service.GetServicesUseCase)
     container.register(service.CreateServiceUseCase)
     container.register(service.EditServiceUseCase)
+    container.register(GetContactsUseCase)
--- src/infrastructure/persistence/repositories/container.py
+++ src/infrastructure/persistence/repositories/container.py
@@ -1,18 +1,29 @@
 from punq import Container
 
-from .base_repository import BaseRepository
+from src.domain.repositories.abstract_booking_repository import (
+    AbstractBookingRepository,
+)
+from src.domain.repositories.abstract_contact_repository import (
+    AbstractContactRepository,
+)
+from src.domain.repositories.abstract_date_repository import AbstractDateRepository
+from src.domain.repositories.abstract_service_repository import (
+    AbstractServiceRepository,
+)
+from src.domain.repositories.abstract_user_repository import AbstractUserRepository
 from .booking_repository import BookingRepository
 from .date_repository import DateRepository
 from .service_repository import ServiceRepository
 from .contact_repository import ContactRepository
+from .user_repository import UserRepository
 
 
 def register(container: Container) -> None:
     """
     Register all repositories in the container.
     """
-    container.register(BaseRepository)
-    container.register(BookingRepository)
-    container.register(ServiceRepository)
-    container.register(DateRepository)
-    container.register(ContactRepository)
+    container.register(AbstractBookingRepository, BookingRepository)
+    container.register(AbstractServiceRepository, ServiceRepository)
+    container.register(AbstractDateRepository, DateRepository)
+    container.register(AbstractContactRepository, ContactRepository)
+    container.register(AbstractUserRepository, UserRepository)
