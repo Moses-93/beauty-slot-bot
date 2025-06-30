ec371e93c2c29417a34fa4850d160fefb8fd3933
Moses-93 <moieseenko2001@gmail.com>
2025-06-30 12:29:02 +0300

    feat: Implement AiopubsubEventBus

---
+++ src/infrastructure/event_bus/aiopubsub_event_bus.py
@@ -0,0 +1,30 @@
+import inspect
+from aiopubsub import Hub, Key, Subscriber, Publisher
+from typing import Type
+
+from src.application.abstractions.event_bus import AbstractEventBus, E, Handler
+
+
+class AiopubsubEventBus(AbstractEventBus):
+    def __init__(self):
+        self._hub = Hub()
+        self._subscriber = Subscriber(self._hub, "main")
+        self._publisher = Publisher(self._hub, "main")
+
+    def _get_key(self, event_type: Type[E]) -> Key:
+        return Key(f"{event_type.__module__}.{event_type.__name__}")
+
+    def subscribe(self, event_type: Type[E], handler: Handler):
+        if not inspect.iscoroutinefunction(handler):
+            raise TypeError(
+                f"Handler for {event_type} must be async"
+            )  # TODO: Add custom exception - InvalidEventHandlerTypeError
+        key = self._get_key(event_type)
+        self._subscriber.add_async_listener(key, handler)
+
+    def publish(self, event: E) -> None:
+        key = self._get_key(type(event))
+        self._publisher.publish(key, event)
+
+    async def reset(self) -> None:
+        await self._subscriber.remove_all_listeners()
