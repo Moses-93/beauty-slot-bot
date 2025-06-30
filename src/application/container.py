6a8836c188402205d7ce9e954099f325f3404daf
Moses-93 <moieseenko2001@gmail.com>
2025-06-30 19:43:44 +0300

    feat: Register event handlers in the DI container

--- src/application/container.py
+++ src/application/container.py
@@ -2,6 +2,7 @@ from punq import Container
 
 from .services import container as services_di
 from .use_cases import container as use_cases_di
+from .event_handlers import container as event_handlers_di
 
 
 def register(container: Container) -> None:
@@ -10,3 +11,4 @@ def register(container: Container) -> None:
     """
     services_di.register(container)
     use_cases_di.register(container)
+    event_handlers_di.register(container)
