7781ddd89623386b41813b2e6a91afe5da69003f
Moses-93 <moieseenko2001@gmail.com>
2025-07-01 16:45:47 +0300

    refactor: Update Booking entity to use Service and DateSlot types

--- src/domain/entities/booking.py
+++ src/domain/entities/booking.py
@@ -1,16 +1,16 @@
 from dataclasses import dataclass, field
 from datetime import time as Time, datetime
 from typing import Optional
-from src.application.dto.date import DateDTO
-from src.application.dto.service import ServiceDTO
+from .service import Service
+from .date import DateSlot
 
 
 @dataclass
 class Booking:
     id: Optional[int] = field(default=None)
-    user_id: int = field(default=None)
-    service: ServiceDTO = field(default=None)
-    date: DateDTO = field(default=None)
-    time: Time = field(default=None)
+    user_id: int
+    service: Service
+    date: DateSlot
+    time: Time
     reminder_time: Optional[datetime] = field(default=None)
     is_active: bool = field(default=True)
