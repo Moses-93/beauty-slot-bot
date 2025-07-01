e2b98380643d70fa00a39d26611d9af4bdc5b0a6
Moses-93 <moieseenko2001@gmail.com>
2025-07-01 16:57:46 +0300

    feat: Add business logic to the Booking entity.
    - The should_schedule_reminder method checks whether there is a reminder
    - The is_past method checks whether the booking is in the past
    - The cancel method confirms that the booking can be canceled by checking its status
    - The confirm method confirms the booking using the is_past method

--- src/domain/entities/booking.py
+++ src/domain/entities/booking.py
@@ -13,4 +13,28 @@ class Booking:
     date: DateSlot
     time: Time
     reminder_time: Optional[datetime] = field(default=None)
-    is_active: bool = field(default=True)
+    is_active: bool = field(default=False)
+
+    def should_schedule_reminder(self, now: datetime) -> bool:
+        return self.reminder_time is not None and now < self.reminder_time
+
+    def is_past(self, now: Optional[datetime] = None) -> bool:
+        """
+        Check if the booking is in the past, taking both date and time into account.
+        """
+        now = now or datetime.now()
+        return datetime.combine(self.date.date, self.time) < now
+
+    def confirm(self) -> None:
+        """
+        Confirm the booking if it is not in the past.
+        """
+        if self.is_past():
+            raise ValueError("...")  # TODO: Add custom exception and message
+        self.is_active = True
+
+    def cancel(self) -> None:
+        """Cancel the booking if it is active."""
+        if not self.is_active:
+            raise ValueError("...")  # TODO: Add custom exception and message
+        self.is_active = False
