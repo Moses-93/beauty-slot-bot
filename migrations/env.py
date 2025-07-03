7e228838d226a9bb0265d5447a4d86f60259c12c
Moses-93 <moieseenko2001@gmail.com>
2025-07-03 11:50:35 +0300

    refactor: Correct import path for Base model in migrations environment

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from src.infrastructure.persistence.models import Base
+from src.infrastructure.persistence.models.base import Base
 from src.core.config import get_settings
 
 
