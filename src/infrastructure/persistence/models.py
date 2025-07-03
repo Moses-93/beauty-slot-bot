ec72fb4d143fe00fcad12033cf3e34e900d14b22
Moses-93 <moieseenko2001@gmail.com>
2025-07-03 11:17:14 +0300

    feat: Add TimeSlot model

--- src/infrastructure/persistence/models.py
+++ src/infrastructure/persistence/models.py
@@ -20,6 +20,26 @@ from src.domain.enums.user_role import UserRole
 Base = declarative_base()
 
 
+class TimeSlotModel(Base):
+    __tablename__ = "time_slots"
+    id = Column(Integer, primary_key=True, index=True)
+    master_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    date = Column(Date, nullable=False)
+    start_time = Column(Time(True), nullable=False)
+    end_time = Column(Time(True), nullable=False)
+    is_active = Column(Boolean, default=True, index=True)
+    is_booked = Column(Boolean, default=False, index=True)
+
+    created_at = Column(DateTime(True), nullable=False, default=func.now())
+    updated_at = Column(
+        DateTime(True), nullable=False, default=func.now(), onupdate=func.now()
+    )
+
+    booking = relationship("BookingModel", back_populates="time_slot", uselist=False)
+
+
 class DateModel(Base):
     __tablename__ = "dates"
     id = Column(Integer, primary_key=True, index=True)
@@ -64,7 +84,7 @@ class BookingModel(Base):
         DateTime(True), nullable=False, default=func.now(), onupdate=func.now()
     )
 
-    date = relationship("DateModel", back_populates="bookings", uselist=False)
+    time_slot = relationship("TimeSlotModel", back_populates="booking", uselist=False)
     service = relationship("ServiceModel", back_populates="bookings", uselist=False)
     user = relationship("UserModel", back_populates="bookings", uselist=False)
 
