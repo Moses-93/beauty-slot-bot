7522fc10a7caade3abbcd36373f00e2e3e6cc2f7
Moses-93 <moieseenko2001@gmail.com>
2025-07-03 16:04:24 +0300

    refactor: Update TimeSlot entity
    - Added the kw_only parameter
    - Added the id, date, is_active, and is_booked fields.
    - Added the can_fit method to check if the duration of the service matches the time slot

--- src/domain/entities/time.py
+++ src/domain/entities/time.py
@@ -1,17 +1,31 @@
-from dataclasses import dataclass
-from datetime import time, timedelta, datetime, date
+from dataclasses import dataclass, field
+from datetime import time, timedelta, datetime, date as Date
+from typing import Optional
 
 
-@dataclass(frozen=True)
+@dataclass(kw_only=True)
 class TimeSlot:
+    id: Optional[int] = field(default=None)
+    date: Date
     start: time
     end: time
+    is_active: bool = field(default=True)
+    is_booked: bool = field(default=False)
 
     @classmethod
-    def create_with_duration(cls, start: time, duration: timedelta) -> "TimeSlot":
+    def from_start_and_duration(
+        cls, date: Date, start: time, duration: timedelta
+    ) -> "TimeSlot":
         return cls(
-            start=start, end=(datetime.combine(date.today(), start) + duration).time()
+            date=date,
+            start=start,
+            end=(datetime.combine(date, start) + duration).time(),
         )
+
+    def can_fit(self, duration: timedelta) -> bool:
+        full_start = datetime.combine(self.date, self.start)
+        full_end = datetime.combine(self.date, self.end)
+        return full_start + duration <= full_end
 
     def overlaps(self, other: "TimeSlot") -> bool:
         return self.start < other.end and other.start < self.end
