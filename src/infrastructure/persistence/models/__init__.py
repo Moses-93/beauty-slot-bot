803724dc4150b3a2ea323ca4bec1e9e34d098500
Moses-93 <moieseenko2001@gmail.com>
2025-07-03 11:52:13 +0300

    refactor: Initialize models package with Booking, Service, User, and Contact models

---
+++ src/infrastructure/persistence/models/__init__.py
@@ -0,0 +1,11 @@
+from .booking import BookingModel, TimeSlotModel
+from .service import ServiceModel
+from .user import UserModel, ContactModel
+
+__all__ = [
+    "BookingModel",
+    "TimeSlotModel",
+    "ServiceModel",
+    "UserModel",
+    "ContactModel",
+]
