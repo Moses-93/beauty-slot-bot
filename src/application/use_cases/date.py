0b68a5adc6fa270b7e0ffc20bea596a89fa1a168
Moses-93 <moieseenko2001@gmail.com>
2025-07-04 14:13:09 +0300

    refactor: Update CreateTimeSlotUseCase to create TimeSlot

--- src/application/use_cases/date.py
+++ src/application/use_cases/date.py
@@ -5,9 +5,10 @@ from src.domain.repositories.abstract_time_slot_repository import (
     AbstractTimeSlotRepository,
 )
 from src.domain.entities.time import TimeSlot
-from src.infrastructure.celery.tasks.deactivation.date import deactivate
+from src.domain.enums.time_slot import TimeSlotStatus
 from src.application.dto.time import TimeSlotDTO
 from src.shared.dto.result import ResultDTO
+from src.infrastructure.celery.tasks.deactivation.date import deactivate
 
 
 class CreateTimeSlotUseCase:
@@ -15,7 +16,16 @@ class CreateTimeSlotUseCase:
         self._time_slot_repo = time_slot_repo
 
     async def __call__(self, time_slot_dto: TimeSlotDTO) -> ResultDTO[TimeSlot]:
-        created_time_slot = await self._time_slot_repo.create(time_slot_dto)
+
+        time_slot = TimeSlot(
+            master_id=time_slot_dto.master_id,
+            date=time_slot_dto.date,
+            start=time_slot_dto.start,
+            end=time_slot_dto.end,
+            status=TimeSlotStatus.AVAILABLE,
+        )
+
+        created_time_slot = await self._time_slot_repo.create(time_slot)
 
         if created_time_slot is None:
             return ResultDTO.fail()
