9682fda3581f0e118a0ea81b2c2fd254a2e7be27
Moses-93 <moieseenko2001@gmail.com>
2025-07-04 09:31:23 +0300

    refactor: Replace Pydantic model with dataclass for ServiceDTO

--- src/application/dto/service.py
+++ src/application/dto/service.py
@@ -1,12 +1,11 @@
-from pydantic import BaseModel, Field
-from typing import Optional
+from dataclasses import dataclass, field
 from datetime import timedelta
 
 
-class ServiceDTO(BaseModel):
-    id: Optional[int] = Field(default=None)
+@dataclass(frozen=True)
+class ServiceDTO:
     master_id: int
     title: str
     price: int
     duration: timedelta
-    is_active: bool = Field(default=True)
+    is_active: bool = field(default=True)
