821a5486d7b01376df91a0a8b48fa096fb921b09
Moses-93 <moieseenko2001@gmail.com>
2025-07-04 19:04:45 +0300

    refactor: Adapt handler method names to ensure consistency after renaming the module

--- src/bot/master/handlers/time_slot.py
+++ src/bot/master/handlers/time_slot.py
@@ -13,34 +13,43 @@ class CreateTimeSlotHandler:
     def __init__(self, container: Container):
         self._container = container
 
-    async def handle_start_add_date(self, message: Message, state: FSMContext):
+    async def handle_add_time_slot(self, message: Message, state: FSMContext):
         await state.set_state(CreateTimeSlotStates.date)
-        await message.answer(text=...)
+        await message.answer(
+            text=...
+        )  # TODO: Add a message prompting the user to set a date
 
     async def handle_set_date(self, message: Message, state: FSMContext, date: str):
         await state.set_state(CreateTimeSlotStates.start_time)
         await state.update_data(date=date)
-        await message.answer(text=...)
+        await message.answer(
+            text=...
+        )  # TODO: Add a message prompting the user to set a start time
 
-    async def handle_set_deactivation_time(
-        self, message: Message, state: FSMContext, deactivation_time: str
+    async def handle_set_start_time(
+        self, message: Message, state: FSMContext, start_time: str
     ):
-        await state.update_data(deactivation_time=deactivation_time)
+        await state.set_state(CreateTimeSlotStates.end_time)
+        await state.update_data(start_time=start_time)
+        await message.answer(
+            text=...
+        )  # TODO: Add a message prompting the user to set an end time
 
-    async def _add_date(self, date: str, deactivation_time: str):
-        pass
+    async def handle_set_end_time(
+        self, message: Message, state: FSMContext, end_time: str
+    ):
+        await state.update_data(end_time=end_time)
 
 
 class CancelTimeSlotHandler:
     def __init__(self, container: Container):
         self._container = container
 
-    async def handle_start_deactivate_date(self, message: Message, state: FSMContext):
+    async def handle_start_cancel_time_slot(self, message: Message, state: FSMContext):
         await state.set_state(CancelTimeSlotStates.time_slot_id)
-        await message.answer(text=..., reply_markup=...)
+        await message.answer(
+            text=..., reply_markup=...
+        )  # TODO Add a message prompting the user to select a time slot to cancel
 
-    async def handle_delete_date(self, callback: CallbackQuery, state: FSMContext):
-        pass
-
-    async def _deactivate_date(self, date_id: int):
+    async def handle_cancel_time_slot(self, callback: CallbackQuery, state: FSMContext):
         pass
